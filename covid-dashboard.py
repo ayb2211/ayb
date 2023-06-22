b105a1b5fd9360e44699a249aca6554713e2b505
ayb2211 <90357276+ayb2211@users.noreply.github.com>
2023-06-22 01:55:12 +0100

    Update covid-dashboard.py

--- covid-dashboard.py
+++ covid-dashboard.py
@@ -27,7 +27,7 @@ all_options = st.sidebar.checkbox("Select all options")
 
 if all_options:
     continent = df["continent"].unique()
-    continent = st.sidebar.selectbox(index=-1)
+    continent=st.sidebar.selectbox("Select Continent", df["continent"].unique(), index=None)
 
 
 st.write('You selected:', continent)
