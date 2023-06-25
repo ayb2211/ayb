2770ce63c5f51783668c48f2a0cdb52ae9a6c9fa
ayb2211 <90357276+ayb2211@users.noreply.github.com>
2023-06-25 15:07:38 +0100

    Update covid-dashboard.py

--- covid-dashboard.py
+++ covid-dashboard.py
@@ -62,7 +62,7 @@ def home() :
     col2.metric(label="Total Deaths", value=f"{s2}")
     col3.metric(label="Total Vaccinations", value=f"{s3}")
     col4.metric(label="People Vaccinated", value=f"{s4}")
-    st.markdown("""---""")
+    st.markdown("""   """)
     
     #2d row
     col5, col6 = st.columns((7,3), gap='large')
