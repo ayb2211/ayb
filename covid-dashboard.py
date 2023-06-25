d425f056726afd3e911bb41ff609cb0e8c349617
ayb2211 <90357276+ayb2211@users.noreply.github.com>
2023-06-25 03:28:15 +0100

    Update covid-dashboard.py

--- covid-dashboard.py
+++ covid-dashboard.py
@@ -48,7 +48,7 @@ df_filter=df.query(
 def home() :
     s1 = int(df_filter['new_cases_smoothed'].sum())
     s2 = int(df_filter['new_deaths'].sum())
-    s3 = df.where((df['location'].isin(location)) & (df['continent'].isin(continent)))
+    s3 = df[(df['location'].isin(location)) & (df['continent'].isin(continent))]
     
     dates = pd.to_datetime(df_filter['date'], format='%Y-%m-%d').dt.date
     #1st Row
