4b54ce4b918c5f42d67fd59b9c430abe890bf01c
ayb2211 <90357276+ayb2211@users.noreply.github.com>
2023-06-25 14:51:00 +0100

    Update covid-dashboard.py

--- covid-dashboard.py
+++ covid-dashboard.py
@@ -1,72 +1,83 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from numerize.numerize import numerize
+import time
+import altair as alt
 import plotly.graph_objects as go
 
-st.set_page_config(page_title="Dashboard", page_icon="🌍", layout="wide")
+st.set_page_config(page_title="Dashboard",page_icon="🌍",layout="wide")
 st.subheader("🔔  Analytics Dashboard")
 st.markdown("##")
+theme_plotly = None
 
-# Load the data from the XLSX file
-@st.cache
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+
+@st.cache_data
 def load_model(model_name):
-    df = pd.read_excel(model_name, sheet_name='Sheet1', parse_dates=['date'])
-    return df
+    df= pd.read_excel(model_name, sheet_name='Sheet1', parse_dates=['date'])
+    return (df)
 
-# Load xlsx file
-df = load_model("owid-covid-data.xlsx")
 
-# Sidebar
+#load xlsx file
+df=load_model("owid-covid-data.xlsx")
+
+#side bar
 st.sidebar.header("Please filter")
-
-continent = st.sidebar.multiselect(
+continent=st.sidebar.multiselect(
     "Select Continent",
     df["continent"].unique(),
     default=None,
 )
-
 all_options_con = st.sidebar.checkbox("Select all options", value=True)
 if all_options_con:
     continent = df["continent"].unique()
 
-location = st.sidebar.multiselect(
+location=st.sidebar.multiselect(
     "Select Location",
     df["location"].unique(),
     default=None,
 )
-
 all_options_loc = st.sidebar.checkbox("Select all options", value=True, key='ch2')
 if all_options_loc:
     location = df["location"].unique()
+df_filter=df.query(
+    "continent in @continent & location in @location"
+)
+st.sidebar.subheader('Line chart parameters')
+plot_data = st.sidebar.multiselect('Select data', ['total_cases', 'total_deaths'], ['total_cases', 'total_deaths'])
+plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
 
-df_filter = df.query("continent in @continent & location in @location")
+def home() :
+    s1 = int(df[(df['location'].isin(location)) & (df['continent'].isin(continent))].groupby('location').max()['total_cases'].sum())
+    s2 = int(df[(df['location'].isin(location)) & (df['continent'].isin(continent))].groupby('location').max()['total_deaths'].sum())
+    s3 = int(df[(df['location'].isin(location)) & (df['continent'].isin(continent))].groupby('location').max()['total_vaccinations'].sum())
+    s4 = int(df[(df['location'].isin(location)) & (df['continent'].isin(continent))].groupby('location').max()['people_vaccinated'].sum())
 
-# Home
-def home():
-    s1 = df_filter['total_cases'].sum()
-    s2 = df_filter['total_deaths'].sum()
-    s3 = df_filter['total_vaccinations'].sum()
-    s4 = df_filter['people_vaccinated'].sum()
-
-    # 1st Row
+    #1st Row
     col1, col2, col3, col4 = st.columns(4)
     col1.metric(label="Total Cases", value=f"{s1}")
     col2.metric(label="Total Deaths", value=f"{s2}")
     col3.metric(label="Total Vaccinations", value=f"{s3}")
     col4.metric(label="People Vaccinated", value=f"{s4}")
-
-    # 2nd Row
-    col5, col6 = st.columns((7, 3))
+    
+    #2d row
+    col5, col6 = st.columns((7,3))
     num_points = 1000  # Number of data points to display
-    sampled_data = df_filter.sample(num_points)
+    sampled_data = df.sample(num_points)
     with col5:
-        st.markdown('### Line Chart')
-        fig = go.Figure()
-        for location in sampled_data['location'].unique():
-            location_data = sampled_data[sampled_data['location'] == location]
-            fig.add_trace(go.Scatter(x=location_data['date'], y=location_data['total_cases'], name=location))
-        fig.update_layout(width=700, height=500)
-        st.plotly_chart(fig)
-
+        st.markdown('### Heatmap')
+        chart = alt.Chart(sampled_data).mark_line().encode(
+            x='date',
+            y='total_cases',
+        ).properties(
+            width=300, height=500
+        )
+        
+        # Render the chart using Streamlit
+        st.altair_chart(chart, use_container_width=True)
     with col6:
         # Group data by continent and calculate the sum of total_deaths
         grouped_data = df_filter.groupby('continent')['total_deaths'].sum().reset_index()
@@ -82,4 +93,4 @@ def home():
         # Render the chart using Streamlit
         st.plotly_chart(fig)
 
-home()
+   
