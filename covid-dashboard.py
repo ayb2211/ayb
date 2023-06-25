c8290b524b9ee84368a67906e9f64055b1f260e1
ayb2211 <90357276+ayb2211@users.noreply.github.com>
2023-06-25 16:05:49 +0100

    Update covid-dashboard.py

--- covid-dashboard.py
+++ covid-dashboard.py
@@ -64,7 +64,7 @@ def home() :
     st.markdown("""---""")
     
     #2d row
-    col5, col6 = st.columns((5,3), gap='small')
+    col5, col6 = st.columns((5,3), gap='large')
     num_points = 1000  # Number of data points to display
     sampled_data = df.sample(num_points)
     
